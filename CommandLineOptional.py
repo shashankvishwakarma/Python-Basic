ea5a02e6b0cb36083fbd164345f37fae15e5c54a
Shashank <mail2shashank1683@gmail.com>
2020-08-14 20:44:11 +0530

    added example for CommandLineOptional.py

--- CommandLineOptional.py
+++ CommandLineOptional.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--number1", help="first number")
     parser.add_argument("--number2", help="second number")
-    parser.add_argument("--operation", help="operation")
+    parser.add_argument("--operation", help="operation", choices=["add", "subtract", "multiply"])
 
     args = parser.parse_args()
     print(args.number1)
@@ -13,12 +13,15 @@ if __name__ == "__main__":
 
     n1 = int(args.number1)
     n2 = int(args.number2)
-    
+    result = None
+
     if args.operation == "add":
         result = n1 + n2
     elif args.operation == "subtract":
         result = n1 - n2
     elif args.operation == "multiply":
         result = n1 * n2
+    else:
+        print("unsupported operation")
 
     print("Result is: ", result)
