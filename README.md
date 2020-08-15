43c649e7f33b398c477125e4f3dc104a73867729
Shashank <mail2shashank1683@gmail.com>
2020-08-15 22:36:51 +0530

    added example for skip/selectively run tests in pytest

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python-Basic
 Python 3 Programming Basics
+
+
+# To install pytest module
+pip install pytest
+
+
+# To tun pytest (either of 3 option)
+python -m test
+py.test
+pytest -v
+pytest -v -rxs (to check why tests are skipped)
--- Unittesting_pytest/test_mathlib.py
+++ Unittesting_pytest/test_mathlib.py
@@ -1,6 +1,10 @@
 import mathlib as m
+import pytest
+import sys
 
-
+# @pytest.mark.skip(reason="Ignoring test")
+# @pytest.mark.skipif(sys.version_info < (3,5), reason="Ignoring test")
+@pytest.mark.skipif(sys.version_info > (3,5), reason="Ignoring test")
 def test_calc_total():
     total = m.calc_total(4, 5)
     assert total == 9
@@ -10,9 +14,3 @@ def test_calc_multiply():
     result = m.calc_multiply(10, 3)
     assert result == 30
 
-
-'''
-# to run
-python -m test
-py.test
-'''
