d3551ff3e49375de4590633d49fed5364ad1f435
Shashank <mail2shashank1683@gmail.com>
2020-08-15 22:19:32 +0530

    added example for Modules

---
+++ MyProgram.py
@@ -0,0 +1,9 @@
+import modules.Function as f
+'''
+# incase module is outside the project 
+import sys
+sys.path.append("F:\PycharmProjects\Python-Basic")
+'''
+
+area = f.cal_square_are(50)
+print(area)
---
+++ modules/Function.py
@@ -0,0 +1,7 @@
+
+def cal_triangle_are(base, height):
+    return 1/2*(base*height)
+
+
+def cal_square_are(length):
+    return length * length
